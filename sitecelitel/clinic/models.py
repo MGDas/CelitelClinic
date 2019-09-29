7c879c1f53bc3b33469add51ed0cfa7c3532b18c
MGDas <mgdas051@gmail.com>
2019-09-29 11:31:11 +0300

    Create app Clinic

--- sitecelitel/clinic/models.py
+++ sitecelitel/clinic/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    public = models.BooleanField(default=True, verbose_name='Публикация')
+
+    class Meta:
+        abstract = True
