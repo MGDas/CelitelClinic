c4a260875552916c04e7246650ec29e713cca6a9
MGDas <mgdas051@gmail.com>
2019-10-12 21:30:32 +0300

    Updated admin panel for articles

--- sitecelitel/article/admin.py
+++ sitecelitel/article/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from article.models import Article, Rating
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Article)
 admin.site.register(Rating)
+
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+    summernote_fields = ('preview', 'content',)
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'doctor', 'slug', 'id']
