35509b0bdf431bb08363b66d6dbbdf8df54a7716
MGDas <mgdas051@gmail.com>
2019-10-20 13:06:53 +0300

    Updated admin panel for tags

--- sitecelitel/tag/admin.py
+++ sitecelitel/tag/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from tag.models import ArticleTag, DoctorTag
 
-admin.site.register(ArticleTag)
-admin.site.register(DoctorTag)
+@admin.register(ArticleTag)
+class ArticleTagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'slug', 'header', 'public']
+    list_editable = ['header', 'public']
+
+@admin.register(DoctorTag)
+class DoctorTagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'slug', 'header', 'public']
+    list_editable = ['header', 'public']
