93b480f316f8c89e7afc6bd8c107ab7551ac0cc2
MGDas <mgdas051@gmail.com>
2019-10-20 23:06:27 +0300

    Added new in all pages

--- sitecelitel/clinic/context_processors.py
+++ sitecelitel/clinic/context_processors.py
@@ -1,10 +1,13 @@
 from article.models import Article
 from tag.models import ArticleTag
+from new.models import New
 
 def index(request):
     articles_header = Article.objects.all().order_by('-id')[:2]
     articles_tags = ArticleTag.objects.all().order_by('-id')[:6]
+    news_header = New.objects.all().order_by('-updated')[:3]
     return {
+        "news_header": news_header,
         "articles_header": articles_header,
         "articles_tags": articles_tags,
         }
