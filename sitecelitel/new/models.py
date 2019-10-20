885d3063891449364bf813b9b434fccd98f3da2c
MGDas <mgdas051@gmail.com>
2019-10-20 23:08:38 +0300

    Created news models

--- sitecelitel/new/models.py
+++ sitecelitel/new/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.shortcuts import reverse
+from clinic.models import BaseModel
+from django.utils import timezone
+from sitecelitel.utils import get_photo
 
-# Create your models here.
+class New(BaseModel):
+
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    preview = models.TextField(verbose_name='Превью')
+    content = models.TextField(verbose_name='Контент')
+    image = models.ImageField(upload_to=get_photo, blank=True, verbose_name='Изображение')
+    caption = models.CharField(max_length=255, blank=True, verbose_name='Подпись')
+
+    created = models.DateField(default=timezone.now, verbose_name='Дата создания')
+    updated = models.DateField(default=timezone.now, verbose_name='Дата обновления')
+
+    header = models.BooleanField(default=True, verbose_name='Показать в шапке')
+
+    doctors = models.ManyToManyField(
+        "doctor.Doctor",
+        related_name='news',
+        blank=True,
+        verbose_name='Доктора'
+    )
+
+    class Meta:
+        db_table = 'news'
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('news_detail_url', kwargs={
+            'year': self.updated.year,
+            'month': self.updated.strftime('%m'),
+            'day': self.updated.strftime('%d'),
+            'pk': self.pk
+        })
