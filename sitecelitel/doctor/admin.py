a0fc9ec045e82ec2196e91f1a744ec0c437dd0d5
MGDas <mgdas051@gmail.com>
2019-10-27 22:34:33 +0300

    Added redactor for text and admin panel for doctor's video

--- sitecelitel/doctor/admin.py
+++ sitecelitel/doctor/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from doctor.models import (
     Doctor, Specialization, DoctorTiming, Consultation, Review, Video
     )
 
-admin.site.register(Doctor)
+@admin.register(Doctor)
+class DoctorAdmin(SummernoteModelAdmin):
+    summernote_fields = ['content', 'special_note']
+
+@admin.register(Video)
+class VideoAdmin(SummernoteModelAdmin):
+    summernote_fields = ['video']
+    list_display = ['title', 'created', 'updated', 'public']
+    list_editable = ['public']
+
 admin.site.register(Specialization)
 admin.site.register(DoctorTiming)
 admin.site.register(Consultation)
 admin.site.register(Review)
-admin.site.register(Video)
