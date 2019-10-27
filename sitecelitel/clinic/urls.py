221a2e524bb1a1fce565744b39f125f1354095cb
MGDas <mgdas051@gmail.com>
2019-10-27 19:59:22 +0300

    Added url for doctors app

--- sitecelitel/clinic/urls.py
+++ sitecelitel/clinic/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index_url'),
     path('articles/', include('article.urls')),
     path('article-tag/', include('tag.urls')),
+    path('doctors/', include('doctor.urls')),
     path('news/', include('new.urls'))
 ]
