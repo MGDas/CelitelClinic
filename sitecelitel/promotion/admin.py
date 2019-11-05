4cf6a95659c1f886a29285eddadbd6ca47069d86
MGDas <mgdas051@gmail.com>
2019-11-06 00:20:43 +0300

    Add redactor and view in admin panel

--- sitecelitel/promotion/admin.py
+++ sitecelitel/promotion/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from promotion.models import Promotion
+from django_summernote.admin import SummernoteModelAdmin
 
-
-admin.site.register(Promotion)
+@admin.register(Promotion)
+class PromotionAdmin(SummernoteModelAdmin):
+    summernote_fields = ['preview', 'content']
+    list_display = [
+        'title',
+        'note',
+        'data_start',
+        'data_end',
+        'public',
+        'header',
+        'created',
+        'updated'
+    ]
