147581c105201735eaf106868c1e8f0f7fb1d60a
MGDas <mgdas051@gmail.com>
2019-11-16 18:39:10 +0300

    GetSerialization

---
+++ sitecelitel/doctor/parsers/GetSpecialization.py
@@ -0,0 +1,33 @@
+import asyncio
+import requests
+import json
+import os
+from doctor.models import Specialization
+
+def connect(url):
+    response = requests.get(url)
+    return response.json()
+
+async def save_in_db(code, name):
+
+    special, status = Specialization.objects.update_or_create(
+        code=code,
+        name=name
+    )
+
+    if not status:
+        print(f"{special}......ОБНОВЛЕНО!")
+    else:
+        print(f"{special}......СОЗДАНО!")
+
+async def main():
+
+    url = 'http://test.studiovector.art/json/GetSpecialization.json'
+    data = connect(url)
+
+    tasks = []
+    for d in data:
+        task = asyncio.create_task(save_in_db(d['code'], d['name']))
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
