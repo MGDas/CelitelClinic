cdc724993bc0d8a3c00239ce2d3b8d209725d764
MGDas <mgdas051@gmail.com>
2019-11-28 00:03:49 +0300

    Created delete for Organizations and others

---
+++ sitecelitel/organization/management/commands/delete_organ.py
@@ -0,0 +1,10 @@
+from doctor.models import Organization, Agreement, Department
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        Agreement.objects.all().delete()
+        Department.objects.all().delete()
+        Organization.objects.all().delete()
+        print('Deleted Organizations!!!')
