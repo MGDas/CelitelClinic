674679a85c0144bfac03c80e20eecaceabce20ec
studiovector <47386966+studiovector@users.noreply.github.com>
2019-12-13 15:54:35 +0300

    Update del_price.py

--- sitecelitel/service/management/commands/del_price.py
+++ sitecelitel/service/management/commands/del_price.py
@@ -1,10 +1,15 @@
 from django.core.management.base import BaseCommand
-from service.models import Price, Service
+from service.models import Price, Service, ServiceGroup, PriceType
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
-
+        PriceType.objects.all().delete()
+        print("All PriceType is deleted!!!")
+        Price.objects.all().delete()
+        print("All Price is deleted!!!")
         Service.objects.all().delete()
-        print("All prices is deleted!!!")
+        print("All Services is deleted!!!")
+        ServiceGroup.objects.all().delete()
+        print("All ServiceGroup is deleted!!!")
