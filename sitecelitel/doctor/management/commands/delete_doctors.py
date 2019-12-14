18907cd999d31d7d4030c0e3e688013537243cfc
studiovector <47386966+studiovector@users.noreply.github.com>
2019-12-14 18:34:57 +0300

    Update delete_doctors.py

--- sitecelitel/doctor/management/commands/delete_doctors.py
+++ sitecelitel/doctor/management/commands/delete_doctors.py
@@ -1,10 +1,19 @@
-from doctor.models import Doctor, Specialization
+from doctor.models import Doctor, Specialization, Timing, DoctorTiming
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
 
-        Doctor.objects.all().delete()
+        # Doctor.objects.all().delete()
         Specialization.objects.all().delete()
-        print('Deleted Specializations!!!')
+        print("Specializations deleted!")
+        Timing.objects.all().delete()
+        print("Timings deleted!")
+        DoctorTiming.objects.all().delete()
+        print("DoctorTimings deleted!")
+        print("===================")
+        print("===================")
+        print("===================")
+        print("===================")
