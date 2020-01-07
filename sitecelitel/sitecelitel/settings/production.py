80b35897716516208d764353148de0cf337ba9d1
studiovector <studiovector.art@yandex.ru>
2020-01-07 16:09:19 +0300

    Update dev

--- sitecelitel/sitecelitel/settings/production.py
+++ sitecelitel/sitecelitel/settings/production.py
@@ -6,9 +6,9 @@ ALLOWED_HOSTS = ['*']    # your site domen_name
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',   # Backend for connection with Data Base
-        'NAME': 'studio3f_test2',                          # Data Base name
-        'USER': 'studio3f_test2',                          # Name user
-        'PASSWORD': 'studio3f_test2',                      # User's password
+        'NAME': 'studio3f_cel2020',                          # Data Base name
+        'USER': 'studio3f_cel2020',                          # Name user
+        'PASSWORD': 'spR5v&FM',                      # User's password
         'HOST': 'localhost',                          # Host for connection to Data Base
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
