fa76bf73de144e612c9138b0ddbb5ba85151e003
MGDas <mgdas051@gmail.com>
2020-01-08 21:40:50 +0300

    Sorted for slider

--- sitecelitel/clinic/admin.py
+++ sitecelitel/clinic/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from clinic.models import Slider
+from adminsortable2.admin import SortableAdminMixin
 
 
 @admin.register(Slider)
-class SliderAdmin(admin.ModelAdmin):
-    list_display = ['title', 'url', 'public']
+class SliderAdmin(SortableAdminMixin, admin.ModelAdmin):
+    list_display = ['title', 'url', 'public', 'order']
+    list_editable = ['public']
