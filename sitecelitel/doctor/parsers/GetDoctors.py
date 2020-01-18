8f142ab81bf1dc6dc17486886ea9dfd7fd3272f7
MGDas <mgdas051@gmail.com>
2020-01-18 16:02:13 +0300

    Updated parsers for doctors

--- sitecelitel/doctor/parsers/GetDoctors.py
+++ sitecelitel/doctor/parsers/GetDoctors.py
@@ -1,109 +1,80 @@
 import asyncio
 import requests
+
 from doctor.models import Doctor, Specialization
 from organization.models import Department
+
+from doctor.parsers.GetDoctorsWithMainCode import main as doctor_with_main_code
+
 
 
 def connect(url):
     response = requests.get(url)
     return response.json()
 
-def special(special_code):
+def get_special(spec_data):
 
-    special_list = []
-    if special_code:
-        for code in special_code:
+    spec_id_list = []
+    if spec_data:
+        for code in spec_data:
             try:
-                special = Specialization.objects.get(code=code)
-                special_list.append(special)
+                special = Specialization.objects.only('id').get(code=code)
+                spec_id_list.append(special)
             except Specialization.DoesNotExist:
                 continue
 
 
-    return special_list
+    return spec_id_list
 
-
-async def save_in_db(code, main_code, full_name, adult, childish, gender, depart_code, special_code):
+def get_depart(depart_data):
 
     try:
-        department = Department.objects.get(code=depart_code)
+        depart = Department.objects.get(code=depart_data)
     except Exception as e:
-        department = False
+        depart = False
 
-    special_list = special(special_code)
-
-    if main_code:
-        try:
-            doctor_with_main_code = Doctor.objects.get(main_code=main_code)
-            if department and department not in doctor_with_main_code.department.all():
-                doctor_with_main_code.department.add(department)
-
-            if special_list:
-                for spec in special_list:
-                    if spec not in doctor_with_main_code.specialization.all():
-                        doctor_with_main_code.specialization.add(spec)
-            doctor_with_main_code.save()
-            print(f"{doctor_with_main_code}......ОБНОВЛЕНО!")
-
-        except Doctor.DoesNotExist:
-            doctor = Doctor.objects.create(
-                code=code,
-                main_code=main_code,
-                full_name=full_name,
-                gender=gender,
-                adult=adult,
-                childish=childish,
-            )
-            if special_list:
-                doctor.specialization.add(*special_list)
-            if department:
-                doctor.department.add(department)
-
-            doctor.save()
-            print(f"{doctor}......СОЗДАНО!")
+    return depart
 
 
+def save_in_db(**data):
+
+    update_fields = {
+        'full_name': data['full_name'],
+        'gender': data['gender'],
+        'adult': data['adult'],
+        'childish': data['childish'],
+    }
+
+    depart = get_depart(data['department_id'])
+    special_list = get_special(data['specialization_id'])
+
+    doctor, status = Doctor.objects.update_or_create(code=data['code'], defaults=update_fields)
+
+    if depart:
+        doctor.department.add(depart)
+
+    if special_list:
+        doctor.specialization.add(*special_list)
+
+    if not status:
+        print(f"{doctor}......ОБНОВЛЕНО!")
     else:
-        doctor, status = Doctor.objects.update_or_create(
-            code=code,
-            defaults={
-                'main_code': main_code if main_code else '-----',
-                'full_name': full_name,
-                'gender': gender if gender else 'н',
-                'childish': childish,
-                'adult': adult
-                }
-        )
-        if department:
-            doctor.department.add(department)
+        print(f"{doctor}......СОЗДАНО!")
 
-        if special_list:
-            doctor.specialization.add(*special_list)
 
-        if not status:
-            print(f"{doctor}......ОБНОВЛЕНО!")
-        else:
-            print(f"{doctor}......СОЗДАНО!")
-
-async def main():
+def main():
 
     url = 'https://celitel05.ru/json/GetDoctors.json'
     data = connect(url)
 
-    tasks = []
+    count = 0
     for d in data:
-        task = asyncio.create_task(
-            save_in_db(
-                d['code'],
-                d['main_code'],
-                d['full_name'],
-                d['adult'],
-                d['childish'],
-                d['gender'],
-                d['department_id'],
-                d['specialization_id']
-                )
-            )
-        tasks.append(task)
+        if not d['main_code']:
+            save_in_db(**d)
+            count += 1
 
-    await asyncio.gather(*tasks)
+    print("============================")
+    print(f"Doctors saved. Count {count}.")
+    print("============================")
+
+    doctor_with_main_code(data)
